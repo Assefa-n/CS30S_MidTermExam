ab0e11b89b841eefc42c4490a47beb0161e61ced
Assefa-n <assefa-n@webmail.uwinnipeg.ca>
2021-12-07 10:50:35 -0600

    Initial sharing of project

---
+++ MidtermExam_ClassCode_PartB.java
@@ -0,0 +1,35 @@
+/********************************************************************
+ * Programmer:    Naga Assefa
+ * Class:  CS30S
+ *
+ * Assignment: MidtermExam_ClassCode_PartB
+ *
+ * Description: brief description of class
+ ***********************************************************************/
+
+// import libraries as needed here
+
+public class MidtermExam_ClassCode_PartB {
+    //*** Class Variables ***
+    
+    //*** Instance Variables ***
+    
+    //*** Constructors ***
+    
+    /*****************************************
+    * Description: brief description of the methods purpose
+    * 
+    * Interface:
+    * 
+    * @param        each parameter of the method should be listed with an @param
+    * @param        parametername description of parameter
+    * 
+    * @return       any return value will be noted here
+    * ****************************************/
+    
+    
+    //*** Getters ***
+    
+    //*** Setters ***
+    
+} // end of public class
