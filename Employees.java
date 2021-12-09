d333df0829ffe6240eeb988ec424537349a08384
Assefa-n <assefa-n@webmail.uwinnnipeg.ca>
2021-12-09 12:05:26 -0600

    i created the reg pay and over time get

--- Employees.java
+++ Employees.java
@@ -12,24 +12,27 @@
 public class Employees {
     //*** Class Variables ***
          private static int NextID = 1000;
+    // ***Constants******
+         private final double RH = 40;    //the amount of ours there are in regular pay
+
     //*** Instance Variables ***
          private int hours;
          private double paycheck;
          private int id;
     //*** Constructors ***
-    public Employees(){
+       public Employees(){
         id = NextID++; // the Id updating every time
         
         hours = 0;     // the hours an employee works
         paycheck = 0;  // the pay check of an employee depending on hours
-    }// end default/no-arg constructor
+      }// end default/no-arg constructor
        
        public Employees( int h, double p){
            id = NextID++; // the Id updating every time
            
            hours = 0;     // the hours an employee works
            paycheck = 0;  // the pay check of an employee depending on hours
-       }//full arg constructor
+      }//full arg constructor
     /*****************************************
     * Description: Get employee hours worked
     * 
@@ -52,6 +55,7 @@ public class Employees {
     public int getHours(){
         return hours;
     }//end getHours()
+    
     /*****************************************
     * Description: the paycheck of an employee
     * 
@@ -85,6 +89,7 @@ public class Employees {
     public void setPay(double p){
         this.paycheck = p;
     }//end getHours()
+
     /*****************************************
     * Description: The hours an emoloyee has worked
     * 
@@ -96,8 +101,49 @@ public class Employees {
         this.hours = h;
     }//end getHours()
     //*** Others ***
+    /*****************************************
+    * Description: calculate the payccheck for an employe in 
+    *              regulat time
+    * 
+    * Interface:
+    * 
+    * @return       double: regeular pay
+    * //*** Getters ***
+    // spetial  'getters'
+    * ****************************************/
+    public double  getRegularP(){
+        
+        double extra = 0;     // the amount of non over time 
+        
+        double regpay = 0.0;     // the paycheck given to the employes 
+                                 // for working 40 hours
+         extra = getHours() - RH;
+         regpay = (getHours()-extra) * getPay();
+         
+        return regpay;
+    }// end getRP()
     
-    
+    /*****************************************
+    * Description: calculate the payccheck for an employe in 
+    *              regulat time
+    * 
+    * Interface:
+    * 
+    * @return       double: regeular pay
+    * //*** Getters ***
+    // spetial  'getters'
+    * ****************************************/
+    public double  getOvertimeP(){
+        
+        double oth = 0;          // the over time hours
+        double otp = 0;          // the over time pay
+
+         oth = getHours() - RH;
+         otp = oth * getPay() * 1.5;
+         
+         
+        return otp;
+    }// end getRP()
     
     /*****************************************
     * Description: Overide to string
