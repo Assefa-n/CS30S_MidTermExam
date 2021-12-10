87971c4f78be3fb3bd30ec3b70ca88916b2088ac
Assefa-n <assefa-n@webmail.uwinnnipeg.ca>
2021-12-09 19:53:29 -0600

    I finished making the to string format look the way I wanted it to

--- Employees.java
+++ Employees.java
@@ -175,12 +175,12 @@ public class Employees {
         String nl = System.lineSeparator(); 
         StringBuilder St = new StringBuilder();
 
-        St.append(String.format("%-2s %d %s", "ID: ", this.getID(),nl));
-        St.append(String.format("%-2s %d %s", "Hours worked: ",this.getHours(),nl));
-        St.append(String.format("%-2s %s %-5.2f %s","Hourly wage: ","$",this.getWage(),nl));
-        St.append(String.format("%-2s %s %-5.2f %s","Regular pay: ","$",this.getRegPay(),nl)); 
-        St.append(String.format("%-2s %s %-5.2f %s","Overtime pay: ","$",this.getOtPay(),nl)); 
-        St.append(String.format("%-2s %s %-5.2f %s","Gross pay: ","$",this.getGrossPay(),nl)); 
+        St.append(String.format("%-2s %d %s", "ID:", this.getID(),nl));
+        St.append(String.format("%-2s %d %s", "Hours worked:", this.getHours(),nl));
+        St.append(String.format("%-2s %s%.2f %s","Hourly wage:", "$",this.getWage(),nl));
+        St.append(String.format("%-2s %s%.2f %s","Regular pay:", "$",this.getRegPay(),nl)); 
+        St.append(String.format("%-2s %s%.2f %s","Overtime pay:", "$",this.getOtPay(),nl)); 
+        St.append(String.format("%-2s %s%.2f %s","Gross pay:", "$",this.getGrossPay(),nl)); 
         return St.toString();
     }// end toString
 } // end of public class
